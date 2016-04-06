16c30f7ff4e5b49fb9cd19244608019d54727a44
Danyal Rizvi <d.s.rizvi@gmail.com>
2016-04-05 20:35:08 -0500

    data-thief in progress

--- scraper/data-thief.py
+++ scraper/data-thief.py
@@ -29,15 +29,15 @@ logger.info(datetime.now() + "| DB session created.")
 def main():
 
 
-def populateByCity(cityID):
+def populateBylocation(locationID):
 
-	query = session.query(Current).filter(Current.city_id == cityID).order_by('timestamp desc').limit(1)
+	query = session.query(Current).filter(Current.location_id == locationID).order_by('timestamp desc').limit(1)
 
 	if len(current) > 0:
 		current = query[0]
 		page_num = current['page_num']
 		startup_count = current['page_num'] * current['per_page']
-		data = al.get_tags_startups(cityID, page_num)
+		data = al.get_tags_startups(locationID, page_num)
 		startups = data['startups']
 	else:
 		data = al.get_tags_startups(locationID, 1)
@@ -45,33 +45,60 @@ def populateByCity(cityID):
 		current['per_page'] = data['per_page']
 		current['page_num'] = data['page']
 		current['last_page'] = data['last_page']
-		current['city_id'] = cityID
+		current['location_id'] = locationID
 		startups = data['startups']
 		startup_count = 0
 
 	for startup in startups:
-		name = startup['name']
-		location = startup['locations'][0]['display_name']
-	    follower_count = startup['']
-	    num_investors = startup['']
-	    market = startup['markets'][0]['display_name']
-	    product_desc = startup['product_desc']
-	    company_url = startup['company_url']
-	    logo_url = startup['logo_url']
-	    company_type = startup['company_type'][0]['display_name']
+		if not startup['hidden']:
+			startup_id = startup['id']
+			startup_info = buildStartupInfo(startup)
+			startup_obj = Company(**startup_info)
+			populateStartup(startup_obj)
 
+			location_id = startup['location'][0]['id']
+			location_info = buildlocationInfo(location_id)
+			location_obj = Location(**location_info)
 
-def getCompanyInfo(companyID):
-	pass
+def buildStartupInfo(startup):
 
-def getRegionInfo(regionID):
-	pass
+	startup_info = {
+		'name'  		 : startup['name']
+		'location'  	 : startup['locations'][0]['display_name']
+		'follower_count' : startup['follower_count']
+		'num_investors'  : 0
+		'market'  		 : startup['markets'][0]['display_name']
+		'product_desc'   : startup['product_desc']
+		'company_url'  	 : startup['company_url']
+		'logo_url'  	 : startup['logo_url']
+		'company_type'   : startup['company_type'][0]['display_name']
+	}
+
+	return startup_info
+
+def buildlocationInfo(location_id):
+
+	location = al.get_tags(location_id)
+
+	location_info = {
+	    'name' : location['display_name']
+	    'investor_followers' : location['statistics']['investor_followers']
+	    'followers' : location['statistics']['followers']
+	    'num_companies' : 0
+	    'num_people' : 0
+	}
+
+	return location_info
 
 def getPersonInfo(personID):
 	pass
 
-def populateCompany():
-	pass
+def populateStartup(startup_obj):
+	try:
+		session.add(startup_obj)
+		session.commit(startup_obj)
+	except:
+		logger.info(datetime.now() + "| Failed to commit startup object.")
 
 def populateLocation():
 	pass
