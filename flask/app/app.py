2dc36436764ceffea7eb8b37bc85baa975b71882
iamkeeb <amanda.keebler@gmail.com>
2016-04-06 14:12:21 -0500

    trying things

--- flask/app/app.py
+++ flask/app/app.py
@@ -6,6 +6,11 @@ from flask import request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
+#for running tests
+from io import StringIO
+import tests
+from unittest import TextTestRunner, makeSuite
+
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
@@ -24,6 +29,19 @@ def index():
     #     db.session.commit()
     # posts = Post.query.all()
     return render_template('index.html')
+
+@app.route('/tests', methods=['GET'])
+def run_tests():
+    """
+    output: returns the output of unittests
+    """
+    
+    stream = StringIO()
+    runner = TextTestRunner(stream=stream, verbosity=2)
+    suite = makeSuite(tests.TestIDB)
+    runner.run(suite)
+    output = stream.getvalue().output.split('\n')
+    return render_template("tests.html", text=output)
 
 
 if __name__ == '__main__':
